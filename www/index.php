3d1e5e2e887714a0e8952a9787b3a676c2f9ad3a
maier <maier@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-18 14:51:34 +0000

    "project home page" updated.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dirichletreg@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,8 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>DirichletReg &ndash; Dirichlet Regression in R</title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name = "dcterms.language" content="en" />
+<?php
+
+/* TITLE */  
+  $title_txt = "DirichletReg &ndash; Dirichlet Regression in R";
+
+/* DESCRIPTION */  
+  $desc_txt = "This project implements Dirichlet regression models for the analysis of Compositional Data, Rates, Proportions, etc. in R.";
+
+  echo "    <title>$title_txt</title>\n";
+  echo "    <meta name = \"dcterms.title\"     content=\"$title_txt\" />\n";
+  echo "    <meta property = \"og:title\"      content=\"$title_txt\" />\n";
+  echo "    <meta property = \"twitter:title\" content=\"$title_txt\" />\n";
+
+  echo "    <meta name = \"description\"             content=\"$desc_txt\" />\n";
+  echo "    <meta name = \"dcterms.description\"     content=\"$desc_txt\" />\n";
+  echo "    <meta property = \"og:description\"      content=\"$desc_txt\" />\n";
+  echo "    <meta property = \"twitter:description\" content=\"$desc_txt\" />\n";
+
+/* KEYWORDS */
+  $keywords_txt = "DirichletReg,Dirichlet Regression,R,Compositional Data,Compositions,Rates,Proportions,Percentages,Quotas,Shares";
+  echo "    <meta name = \"keywords\"        content = \"$keywords_txt\" />";
+  echo "    <meta name = \"dcterms.subject\" content=\"$keywords_txt\" />\n";
+
+?>
+    <meta name="robots" content="Index,Follow" />
     <link href="style.css" rel="stylesheet" type="text/css">
   </head>
   <body><div id="universe">
@@ -23,6 +48,10 @@
       <strong class="listheader">Project Summary Page</strong>
       <br />
       <a href="http://r-forge.r-project.org/projects/dirichletreg/">http://r-forge.r-project.org/projects/dirichletreg/</a>
+      <br />
+      <strong class="listheader">CRAN Releases</strong>
+      <br />
+      <a href="http://cran.r-project.org/package=DirichletReg">http://cran.r-project.org/package=DirichletReg</a>
     </li>
     <li>
       <strong class="listheader">Publications &amp; Citations</strong>
@@ -41,11 +70,11 @@
           Maier, M. J. (2014). <i>DirichletReg: Dirichlet Regression for Compositional Data in R.</i>
           Research Report Series&nbsp;/ Department of Statistics and Mathematics, 125.
           WU Vienna University of Economics and Business, Vienna.
-          <a href="http://epub.wu.ac.at/4077/", target="_blank">http://epub.wu.ac.at/4077/</a>
+          <a href="http://epub.wu.ac.at/4077/" target="_blank">http://epub.wu.ac.at/4077/</a>
         </li>
       </ol>
     </li>
   </ul>
 
-  </div id="universe"></body>
+  </div></body>
 </html>
